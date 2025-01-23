fef9e1ead5b76f83d8cd228100b06b356248a6eb
jjpd777 <juanjo.palacioduque@gmail.com>
2025-01-22 22:16:07 -0500

    Albertos quotes done

--- bibleApp/constants/verses.tsx
+++ bibleApp/constants/verses.tsx
@@ -134,5 +134,293 @@ export const VERSES = [
     { verse:     'ROM.12.12',
       audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/45-Romanos_12.mp3",
       date: '01/28/2025'
+    },
+    { verse:     'ISA.26.3',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_26.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'EPH.3.20',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/49-Efesios_3.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.37.4',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_037.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'COL.3.23',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/51-Colosenses_3.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'DEU.31.6',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/05-Deuterenomio_31.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.121.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_121.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1PE.5.7',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/60-1_Pedro_5.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'GAL.6.9',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/48-Gálatas_6.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.55.8',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_55.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '2CH.7.14',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/14-2_Crónicas_07.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1CO.15.58',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/46-1_Corintios_15.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.34.8',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_034.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ROM.15.13',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/45-Romanos_15.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PRO.4.23',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/20-Proverbios_04.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.40.29',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_40.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '2CO.4.16',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/47-2_Corintios_04.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.118.24',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_118.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PRO.3.7',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/20-Proverbios_03.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ZEP.3.17',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/36-Sofonías_3.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.43.2',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_43.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.91.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_091.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'EPH.2.10',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/49-Efesios_2.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'MIC.6.8',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/33-Miqueas_6.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.62.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_062.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'HEB.12.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/58-Hebreos_12.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PRO.24.16',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/20-Proverbios_24.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1JN.1.9',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/62-1_Juan_1.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'JHN.16.33',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/43-Juan_16.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'EXO.14.14',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/02-Exodo_14.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'MAT.6.33',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/40-Mateo_06.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ROM.8.31',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/45-Romanos_08.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'REV.21.4',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/66-Apocalipsis_21.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.46.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_046.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.37.5',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_037.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1PE.2.9',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/60-1_Pedro_2.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.12.2',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_12.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'MAT.5.14',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/40-Mateo_05.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PRO.31.25',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/20-Proverbios_31.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ROM.5.3',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/45-Romanos_05.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.63.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_063.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '2CO.5.17',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/47-2_Corintios_05.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.30.21',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_30.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'HEB.10.23',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/58-Hebreos_10.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '2CH.20.17',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/14-2_Crónicas_20.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PHP.1.6',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/50-Filipenses_1.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'EPH.6.10',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/49-Efesios_6.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1TH.5.16',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/52-I_Tesalonicenses_5.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.58.11',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_58.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1SA.16.7',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/09-1_Samuel_16.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'JHN.8.12',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/43-Juan_08.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.119.11',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_119.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'JER.32.27',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/24-Jeremías_32.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.9.9',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_009.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.55.6',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_55.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.19.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_019.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.121.7',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_121.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '1TH.4.16',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/52-I_Tesalonicenses_4.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PRO.16.3',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/20-Proverbios_16.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.32.8',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_032.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'EXO.15.2',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/02-Exodo_15.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ROM.12.2',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/45-Romanos_12.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.18.2',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_018.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.61.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_61.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.126.5',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_126.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'HEB.13.5',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/58-Hebreos_13.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'LUK.6.38',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/42-Lucas_06.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.40.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_040.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ISA.41.1',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/23-Isaías_41.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.27.13',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_027.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'ROM.15.4',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/45-Romanos_15.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     '2CO.1.3',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/47-2_Corintios_01.mp3",
+      date: '01/28/2025'
+    },
+    { verse:     'PSA.84.11',
+      audioPath: "gs://bendiga-4d926.firebasestorage.app/bible_polished/19-Salmos_084.mp3",
+      date: '01/28/2025'
     }
 ]
