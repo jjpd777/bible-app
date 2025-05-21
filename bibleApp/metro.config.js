3597abd8f2eb3676a7fba32731c7741879e9c21e
jjpd777 <juanjo.palacioduque@gmail.com>
2025-05-21 17:55:32 -0500

    Wassup

--- bibleApp/metro.config.js
+++ bibleApp/metro.config.js
@@ -1,9 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add these Firebase-specific configurations
+config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
+
+// Keep your existing transformer config if needed
 config.transformer = {
   ...config.transformer,
   _expoRelativeProjectRoot: __dirname
